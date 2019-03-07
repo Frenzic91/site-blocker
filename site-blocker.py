416fb4252c4aa44a7bb6164a4c00d2a615afc11c
Sandi Milicevic <hivemindcoder@gmail.com>
2019-03-07 00:33:45 -0500

    Changed some var names

--- site-blocker.py
+++ site-blocker.py
@@ -1,9 +1,9 @@
 from time import *
 from datetime import datetime as dt
 
-host_path = r'C:\Windows\System32\drivers\etc\hosts'
+hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 redirect_ip = '127.0.0.1'
-websites = ['www.youtube.com', 'youtube.com']
+website_list = ['www.youtube.com', 'youtube.com']
 
 while True:
     now = dt.now()
