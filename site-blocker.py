fbda4ef0a041db39f587bc4bf57f01e89b002b8b
Sandi Milicevic <hivemindcoder@gmail.com>
2019-03-07 19:27:17 -0500

    Created local hosts file for testing. Implemented writing of blocked websites and the associated redirect ip to the host file if not already written

--- site-blocker.py
+++ site-blocker.py
@@ -1,6 +1,7 @@
 from time import *
 from datetime import datetime as dt
 
+hosts_temp = 'hosts'
 hosts_path = r'C:\Windows\System32\drivers\etc\hosts'
 redirect_ip = '127.0.0.1'
 website_list = ['www.youtube.com', 'youtube.com']
@@ -9,6 +10,13 @@ while True:
     now = dt.now()
     if dt(now.year, now.month, now.day, 8) < now < dt(now.year, now.month, now.day, 16):
         print('Websites are being blocked')
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect_ip + ' ' + website + '\n')
     else:
         print('All websites are available')
 
